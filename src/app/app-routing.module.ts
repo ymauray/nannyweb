7e838909a91bf8a2332196ac6b394e28c0cf0dcf
Yannick Mauray <yannick.mauray@gmail.com>
2022-03-21 01:16:10 +0100

    Added privacy policy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ConfidentialiteComponent } from './confidentialite/confidentialite.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'confidentialite', component: ConfidentialiteComponent },
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConfidentialiteComponent } from './confidentialite/confidentialite.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfidentialiteComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
